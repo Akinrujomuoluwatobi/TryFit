ab0c1081002dd9f64f2615d3616c312c23c6615b
Oluwatobiloba Akinrujomu <tobiak2011@gmail.com>
2025-05-30 10:26:27 +0200

    Initial Commit

---
+++ TryFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryFit
+//
+//  Created by Oluwatobiloba Akinrujomu on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryFit/TryFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryFitApp.swift
+//  TryFit
+//
+//  Created by Oluwatobiloba Akinrujomu on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TryFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
